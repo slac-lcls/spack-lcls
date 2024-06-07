c20e0ecb87ad8c2f204bec7d1bf40ebf69012687
Matthew Avaylon <mavaylon1@berkeley.edu>
2024-06-07 11:54:32 -0700

    Update package.py

--- packages/py-epix-100a-gen2/package.py
+++ packages/py-epix-100a-gen2/package.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2022, The Board of Trustees of the Leland Stanford Junior 
+# University, through SLAC National Accelerator Laboratory (subject to receipt 
+# of any required approvals from the U.S. Dept. of Energy).
+
 from spack.package import *
 
 
