2a3dd150bd7d47b197f748243649b441549631f2
Valerio Mariani <valmar@slac.stanford.edu>
2024-07-01 15:33:10 -0700

    Updated lcls packages to version 4.1.2

--- packages/psdaq/package.py
+++ packages/psdaq/package.py
@@ -13,7 +13,9 @@ class Psdaq(CMakePackage):
     url = "https://github.com/slac-lcls/lcls2/archive/refs/tags/4.0.0.tar.gz"
 
     maintainers("valmar")
-
+  
+    version("4.1.2", sha256="db2cb8f6e4050380fbe71f88afa3d3b4e5d7903d58666c5984fb591c8e06a957")
+    version("4.1.1", sha256="41f99018f13f10020140982e416817b3569de53a0a44ad4dc99f03a5b5ad2ddd")
     version("4.1.0", sha256="f3f2c7850f43a2e0dbed7589ff37a706fe17e4fa07f2878dca47f6b6ea7d677b")
     version("4.0.0", sha256="f38f8c884b688c3b681fbce9be6068078f8549b05f510234a61f985ae1db0cfd")
 
@@ -29,6 +31,7 @@ class Psdaq(CMakePackage):
     depends_on("rdma-core", type=("build", "run"))
     depends_on("libfabric", type=("build", "run"))
     depends_on("libzmq", type=("build", "run"))
+    depends_on("nlohmann-json", type=("build", "run"))
     depends_on("python@3.9", type=("build", "run"))
 
     root_cmakelists_dir = 'psdaq'
--- packages/xtcdata/package.py
+++ packages/xtcdata/package.py
@@ -14,6 +14,8 @@ class Xtcdata(CMakePackage):
 
     maintainers("valmar")
 
+    version("4.1.2", sha256="db2cb8f6e4050380fbe71f88afa3d3b4e5d7903d58666c5984fb591c8e06a957")
+    version("4.1.1", sha256="41f99018f13f10020140982e416817b3569de53a0a44ad4dc99f03a5b5ad2ddd")
     version("4.1.0", sha256="f3f2c7850f43a2e0dbed7589ff37a706fe17e4fa07f2878dca47f6b6ea7d677b")
     version("4.0.0", sha256="f38f8c884b688c3b681fbce9be6068078f8549b05f510234a61f985ae1db0cfd")
 
